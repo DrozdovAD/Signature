bb73f9e759017f8c123c0b46870480353e1332c7
Alexander Drozdov <aleksandr.drozdov@retailrocket.io>
2022-01-26 02:08:21 +0300

    Initial commit

---
+++ SIgnature/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA0001:XML comment analysis is disabled due to project configuration", Justification = "-")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1118:The parameter spans multiple lines", Justification = "Cause it's harder to read the code")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Не используем комментарии, т.к. они устаревают")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable xml documentation output", Justification = "Cause we don't want to use xml documentation")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "AV1555:method is invoked with a named argument", Justification = "Named args are useful.")]
---
+++ SIgnature/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Signature
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+
+    internal static class Program
+    {
+        public static void Main(
+            string[] args)
+        {
+            var filePath = "../../../test.txt";
+            var blockSize = 10;
+
+            try
+            {
+                using var fileStream = new FileStream(
+                    path: filePath,
+                    mode: FileMode.Open,
+                    access: FileAccess.Read);
+
+                var blockNumber = 0;
+
+                while (true)
+                {
+                    var buffer = new byte[blockSize];
+                    var size = fileStream.Read(
+                        buffer: buffer,
+                        offset: 0,
+                        count: blockSize);
+
+                    if (size == 0)
+                    {
+                        return;
+                    }
+
+                    using var sha256 = SHA256.Create();
+                    var hash = sha256.ComputeHash(buffer);
+                    var readableHash = BitConverter.ToString(hash)
+                        .Replace("-", string.Empty);
+
+                    Console.WriteLine(
+                        "Number: {0}, Hash: {1}",
+                        blockNumber++,
+                        readableHash);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unhandled exception: {e.Message}");
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
