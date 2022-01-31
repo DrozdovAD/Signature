0404bcb93037dd8cf1a711afe6297c0e9a0b97ed
Alexander Drozdov <aleksandr.drozdov@retailrocket.io>
2022-01-31 05:50:30 +0300

    take filePath and blockSize from args

--- Signature/Program.cs
+++ Signature/Program.cs
@@ -9,25 +9,47 @@
         public static void Main(
             string[] args)
         {
-            var filePath = "../../../test.txt";
-            var blockSize = 10;
+            ExtractArguments(args,  out var filePath, out var blockSize);
 
             try
             {
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
-
+                // var stopwatch = new Stopwatch();
+                // stopwatch.Start();
                 using var signature = FileSignatureFactory.Create();
                 signature.Process(
                     filePath: filePath,
                     blockSize: blockSize);
 
-                stopwatch.Stop();
-                Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+                // stopwatch.Stop();
+                // Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
             }
             catch (Exception e)
             {
                 Logger.Error(e);
+            }
+        }
+
+        private static void ExtractArguments(
+            string[] args,
+            out string fileName,
+            out int blockSize)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            if (args.Length != 2)
+            {
+                throw new ArgumentException(nameof(args));
+            }
+
+            fileName = args[0];
+            blockSize = int.Parse(args[1]);
+
+            if (blockSize <= 0)
+            {
+                throw new ArgumentException(nameof(blockSize));
             }
         }
     }
